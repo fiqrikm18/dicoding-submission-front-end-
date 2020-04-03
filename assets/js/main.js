8247e8e26d792ea3a38542f8dafa957e742dac27
fiqri khoirul m <fiqrikm18@gmail.com>
2020-04-03 08:51:26 +0700

    add login register page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
 const currentBalanceInfo = document.querySelector("#current-balance");
 const expensesInfo = document.querySelector("#expenses");
+
+function saveData(event) {
+    event.preventDefault();
+
+    console.log(event.target["note"].value);
+    console.log(event.target["amount"].value);
+}
